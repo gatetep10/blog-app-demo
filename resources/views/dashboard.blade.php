a1d7cf27e48212b1d9e85f2579cf91ad28935e18
gatetep10 <p.gatete1@gmail.com>
2023-05-02 09:15:55 +0200

    First commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+@section('title', '| Edit Post')
+@section('contents')
+<div class="container">
+  <div class="row" style = "margin-top: 25px; margin-bottom:45px">
+    <h4>Welcome to the admin dashboard</h4>
+  </div>
+</div>
+
+  @endsection()
+
+  @section('footer')
+     @parent()
+  
+  @endsection()
